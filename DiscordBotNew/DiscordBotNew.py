71f7f3a27fb6a23c444aa69767cb9cba31be6632
Goblerino <teemoubastard@gmail.com>
2021-02-27 09:42:56 +0100

    Status

--- DiscordBotNew/DiscordBotNew.py
+++ DiscordBotNew/DiscordBotNew.py
@@ -3,6 +3,15 @@ import config
 from discord.ext import tasks, commands
 
 client = discord.Client()
-client.run(config.TOKEN)
 
-bot.run(config.TOKEN)
+
+bot = commands.Bot(command_prefix="?")
+
+@client.event
+async def on_ready():
+    print("Bot is connected.")
+    await client.change_presence(status = discord.Status.idle, activity= discord.Game("Type '?help' for help!"))
+
+
+
+client.run(config.TOKEN)
